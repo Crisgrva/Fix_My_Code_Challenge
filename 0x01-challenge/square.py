f5f1f056078d7549cbf59c80df3f829f54b85725
Crisgrva <crstngranada@gmail.com>
2022-02-16 16:50:39 -0500

    [ Task 1 ] Fix square [ 02/16/2022/04:50 ]

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,7 +7,7 @@ by the width in the Area method.
 """
 
 
-class square():
+class Square():
     """
     Square Class with methods to get
     Perimeter and Area
@@ -24,8 +24,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """ Perimeter of the Square """
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -35,7 +34,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
